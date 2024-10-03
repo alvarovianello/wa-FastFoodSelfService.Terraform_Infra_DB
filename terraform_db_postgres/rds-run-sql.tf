b5e6a9f9a664c6b5b69c54367769128a2a235a6f
alvarovianello <alvaro_s_o@hotmail.com>
2024-10-03 01:24:21 -0300

    Update rds-run-sql.tf

--- terraform_db_postgres/rds-run-sql.tf
+++ terraform_db_postgres/rds-run-sql.tf
@@ -3,14 +3,14 @@ resource "null_resource" "run_sql" {
 
   provisioner "local-exec" {
     command = <<EOT
-      sleep 240  # segundos
-      set PGPASSWORD=${var.dbPassword} && psql -h ${aws_db_instance.postgres_db.endpoint} -p 5432 -U ${var.dbUser} -d ${var.dbName} -f DDL.sql
+      sleep 150  # segundos
+      set PGPASSWORD=${var.dbPassword} && psql -h ${aws_db_instance.postgres_db.address} -p 5432 -U ${var.dbUser} -d ${var.dbName} -f DDL.sql
     EOT
   }
 }
 
-output "rds_endpoint" {
-  value = aws_db_instance.postgres_db.endpoint
+output "rds_address" {
+  value = aws_db_instance.postgres_db.address
 }
 
 output "rds_instance_id" {
