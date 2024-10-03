ab186cf5f8b26223056a20afc1f757017aaca80e
alvarovianello <alvaro_s_o@hotmail.com>
2024-10-03 01:04:14 -0300

    Update sg.tf

--- terraform_db_postgres/sg.tf
+++ terraform_db_postgres/sg.tf
@@ -9,6 +9,7 @@ resource "aws_security_group" "sg-fastfood-db" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Egress Rules for RDS
