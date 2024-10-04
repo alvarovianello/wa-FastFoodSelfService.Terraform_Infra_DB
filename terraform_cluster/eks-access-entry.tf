1a382b3c009bb32c0a65691166c71c3b2407d7f2
alvarovianello <alvaro_s_o@hotmail.com>
2024-10-03 23:28:29 -0300

    terraform

--- terraform_cluster/eks-access-entry.tf
+++ terraform_cluster/eks-access-entry.tf
@@ -4,3 +4,8 @@ resource "aws_eks_access_entry" "eks-access-entry-fastfood" {
   kubernetes_groups = ["fastfood-group-kubernetes"]
   type              = "STANDARD"
 }
+
+output "cluster_access_entry_id" {
+  value       = "${var.projectName}#{var.principalArn}"
+  description = "The Access Entry Id"
+}
